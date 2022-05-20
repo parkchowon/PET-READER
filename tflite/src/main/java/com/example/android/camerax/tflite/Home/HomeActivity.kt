c91c22d394544b765ca6144fa0c0537685a2f459
sectionr0 <sectionr0@gmail.com>
2022-05-20 13:29:41 +0900

    홈 추가

---
+++ tflite/src/main/java/com/example/android/camerax/tflite/Home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.camerax.tflite.Home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.example.camerax.tflite.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
